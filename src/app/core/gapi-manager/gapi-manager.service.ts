5a8824705920b8fb04e855ed4c8a9cfd250c3142
Gabriel Munteanu <gabimunteanu.sdt@gmail.com>
2017-09-16 20:13:48 +0200

    Oath google

--- src/app/core/gapi-manager/gapi-manager.service.ts
+++ src/app/core/gapi-manager/gapi-manager.service.ts
@@ -1,74 +1,64 @@
-import { ApiDataHandler } from './../api/api-data-handler';
-import { IAppConfig } from './../../app.config';
-import { Injectable, Inject } from '@angular/core';
-import { GoogleAuthService } from 'ng-gapi/lib/GoogleAuthService';
+import {ApiDataHandler} from './../api/api-data-handler';
+import {IAppConfig} from './../../app.config';
+import {Injectable, Inject} from '@angular/core';
+import {GoogleAuthService} from 'ng-gapi/lib/GoogleAuthService';
 
 @Injectable()
 export class GapiManagerService {
 
-    constructor(
-        private _googleAuthService: GoogleAuthService,
-        private _apiDataHandler: ApiDataHandler,
-        @Inject('AppConfig') private _appConfig: IAppConfig
-    ) {
-    }
+  constructor(private _googleAuthService: GoogleAuthService,
+              private _apiDataHandler: ApiDataHandler,
+              @Inject('AppConfig') private _appConfig: IAppConfig) {
+  }
 
-    public login() {
-        console.log('login');
-        this._googleAuthService.getAuth().subscribe((auth) => {
-            console.log(auth.isSignedIn.get());
-            const currentUser = auth.currentUser.get().getId();
-            console.log(currentUser);
-            if (auth.isSignedIn) {
-                // const currentUser = auth.currentUser.get().getAuthResponse();
-                // console.log(currentUser);
-                // auth.signOut();
-            }
-            auth.signIn().then(res => {
-                const idToken = res.Zi.id_token;
-                // console.log(res);
-                // this.verifyIdToken(idToken).subscribe(
-                //     (result) => console.log(result)
-                // );
-                const accessToken = res.Zi.access_token;
-                console.log(accessToken);
-                this.registerOAuth(accessToken).subscribe(
-                    (result) => console.log(result)
-                );
-            }, err => console.log(err));
-        });
-    }
+  public login() {
+    console.log('login');
+    this._googleAuthService.getAuth().subscribe((auth) => {
+      console.log(auth.isSignedIn.get());
+      const currentUserId = auth.currentUser.get().getId();
+      console.log(currentUserId);
+      auth.signIn().then(res => {
+        const response = res.getAuthResponse();
+        const idToken = response.id_token;
+        const accessToken = response.access_token;
+        this.loginOAuth(idToken, accessToken).subscribe(
+            (result) => console.log(result)
+        );
+      }, err => console.log(err));
+    });
+  }
 
-    public registerOAuth(idToken: String) {
-        const payload = {
-            token: idToken,
-            oauthType: 'google'
-        };
-        return this._apiDataHandler.postApi('oauth/register', payload);
-    }
+  public loginOAuth(idToken: String, accessToken: String) {
+    const payload = {
+      idToken,
+      accessToken,
+      oauthType: 'google'
+    };
+    return this._apiDataHandler.postApi('oauth/login', payload);
+  }
 
-    public verifyIdToken(idToken: String) {
-        const payload = {
-            idToken
-        };
-        return this._apiDataHandler.postApi('oauth/verify', payload);
-    }
+  public verifyIdToken(idToken: String) {
+    const payload = {
+      idToken
+    };
+    return this._apiDataHandler.postApi('oauth/verify', payload);
+  }
 
 
-    public tokenInfo(token: String) {
-        const payload = {
-            token
-        };
-        return this._apiDataHandler.postApi('oauth/tokeninfo', payload);
-    }
+  public tokenInfo(token: String) {
+    const payload = {
+      token
+    };
+    return this._apiDataHandler.postApi('oauth/tokeninfo', payload);
+  }
 
-    public getMe() {
-        console.log('getMe');
-        return;
-    }
+  public getMe() {
+    console.log('getMe');
+    return;
+  }
 
-    public logout() {
-        console.log('logout');
-        return;
-    }
+  public logout() {
+    console.log('logout');
+    return;
+  }
 }
